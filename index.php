238a18f38e22b2b109817b4b82684b3b26565cbf
Giberto López Ambrosino <gilberto.amb@gmail.com>
2016-12-29 10:29:53 +0000

    viesgo config

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 require_once 'vendor/autoload.php';
 
-$smtp_server = '127.0.0.1';
+$smtp_server = '10.122.8.19';
 $smtp_port = 25;
-$smtp_username = '';
-$smtp_password = '';
+$smtp_username = 'termostatoenua@viesgo.com';
+$smtp_password = 'T3m0@E8979';
 
-$form = ['from@127.0.0.1'];
-$to = [''];
+$form = ['termostatoenua@viesgo.com'];
+$to = ['glopez@appforbrands'];
 
 $transport = Swift_SmtpTransport::newInstance($smtp_server, $smtp_port)
     ->setUsername($smtp_username)
